b232e70f0df60ededee816c3812b8e61c043b208
AnjasFedo <fedoaf98@gmail.com>
2024-03-27 23:28:52 +0700

    Obtain User Auth Token

--- django_rest_framework/urls.py
+++ django_rest_framework/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import TestView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', TestView.as_view(), name='testview')
+    path('', TestView.as_view(), name='testview'),
+    path('api/token/', obtain_auth_token, name='obtain_auth_token')
 ]
