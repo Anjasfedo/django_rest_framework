a3a125557bb4e03fabca3bb1fbd1b94bad1b560e
AnjasFedo <fedoaf98@gmail.com>
2024-03-27 22:47:41 +0700

    Serializers of StudentSerializer

--- drfapp/serializers.py
+++ drfapp/serializers.py
@@ -1,1 +1,7 @@
+from rest_framewok import serializers
+from .models import Student
 
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('name', 'age')
